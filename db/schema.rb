6dc50abfcb1e8a475015888f2c9d4ad5e633f362
redockirtap <redockirtap@gmail.com>
2023-03-26 21:37:48 +0300

    fix: create categories table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_26_171520) do
+ActiveRecord::Schema.define(version: 2023_03_26_183459) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2023_03_26_171520) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "correct", default: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
@@ -33,6 +40,7 @@ ActiveRecord::Schema.define(version: 2023_03_26_171520) do
     t.integer "level", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
