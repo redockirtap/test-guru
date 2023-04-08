0ed1d977ebe653dc79c64c2075293fbb3673e942
redockirtap <redockirtap@gmail.com>
2023-04-08 22:30:24 +0300

    Add new columns to users table and rearrange existing columns.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_08_164322) do
+ActiveRecord::Schema.define(version: 2023_04_08_191105) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -53,7 +53,9 @@ ActiveRecord::Schema.define(version: 2023_04_08_164322) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
+    t.string "name", null: false
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
