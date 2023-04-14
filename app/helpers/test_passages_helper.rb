395300d3c0541dbcbce52e4ddd11272fd7de2ccc
redockirtap <redockirtap@gmail.com>
2023-04-14 23:26:47 +0300

    add methods to test passage helper, complete views

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,19 @@
 module TestPassagesHelper
+  def score_count(resource)
+    total = ((resource.correct_questions / resource.test.questions.count.to_f) * 100).round
+
+    if total <= 65
+      "<p class=\"test--failed\">You scored #{total}%. Test should be retaken.</p>".html_safe
+    else
+      "<p class=\"test--failed\">You scored #{total}%. Test is passed successfully!</p>".html_safe
+    end
+  end
+
+  def questions_count(resource)
+    "#{current_question_number(resource)} out of #{@test_passage.test.questions.count}"
+  end
+
+  def current_question_number(resource)
+    resource.test.question_ids.find_index(resource.current_question.id) + 1
+  end
 end
