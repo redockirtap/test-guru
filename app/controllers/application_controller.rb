331c93c48bac2ff6606fc1ed63d02c64300d6216
redockirtap <redockirtap@gmail.com>
2023-04-15 20:47:22 +0300

    set up devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  helper_method :current_user, :logged_in?
-
-  private
-
-  def authenticate_user!
-    unless current_user
-      cookies[:back_to_page] = request.url
-      redirect_to login_path, alert: "Are you a Guru? Something's wrong with email/password."
-    end
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :trackable, :confirmable
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :tests_author, class_name: 'Test', foreign_key: :author_id
 
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  has_secure_password
 
   def filter_tests_by_level(level)
     tests.where(level:)
