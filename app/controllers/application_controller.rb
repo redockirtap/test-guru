41c5194b4b22fef4f7830587fde01a6a19d370bc
redockirtap <redockirtap@gmail.com>
2023-04-15 16:59:03 +0300

    add session creation on login and resgister

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   private
 
   def authenticate_user!
-    redirect_to login_path unless current_user
+    redirect_to login_path, alert: "Are you a Guru? Something's wrong with email/password." unless current_user
   end
 
   def current_user
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -57,6 +57,6 @@ class TestsController < ApplicationController
   end
 
   def set_user
-    @user = User.first
+    @user = current_user
   end
 end
