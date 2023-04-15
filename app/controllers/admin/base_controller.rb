209932bbb0bfff6a2165cd20717c9e87376830d1
redockirtap <redockirtap@gmail.com>
2023-04-15 22:57:28 +0300

    separate tests views for admin and user, add admin panel

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,13 @@
+class Admin::BaseController < ApplicationController
+
+  layout 'admin'
+
+  before_action :authenticate_user!
+  before_action :authorize_admin!
+
+  private
+
+  def authorize_admin!
+    redirect_to root_path, alert: 'Admins only!' unless current_user.is_a?(Admin)
+  end
+end
