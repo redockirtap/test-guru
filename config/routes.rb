51c4fe4aaca21923724fe99b0d964872574fb838
redockirtap <redockirtap@gmail.com>
2023-04-15 14:31:12 +0300

    create sessions resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root to: 'tests#index'
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
 
   resources :users, only: :create
+  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
