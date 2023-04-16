644cef6cefe2d759d6019f503803c54ced9ecfe5
redockirtap <redockirtap@gmail.com>
2023-04-16 22:27:21 +0300

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
+  get '/locale', to: 'application#change_locale'
+
   resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
